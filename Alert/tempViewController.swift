0d7913354b8a561c45fc45aea356049549daadc2
MisoJei <wnsduddnl@gmail.com>
2023-12-14 01:37:16 +0900

    Add module(div)

--- Alert/tempViewController.swift
+++ Alert/tempViewController.swift
@@ -35,7 +35,7 @@ class tempViewController: UIViewController {
     }
     
     func mul(a: Int, b: Int) -> Int {
-        return a*b // div
+        return a*b
     }
     
     func div(a: Int, b: Int) -> Int {
